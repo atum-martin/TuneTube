0736cbf5ffb836699de2197679a6a57ea563e3f2
atum-martin <martin.chapman4@mail.dcu.ie>
2017-05-12 18:27:49 +0100

    Added basic HTTP connector for use with youtube. Replicates Chrome headers and the orders they are sent in.

---
+++ app/src/main/java/com/atum/tunetube/youtube/YoutubeHttp.java
@@ -0,0 +1,38 @@
+package com.atum.tunetube.youtube;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by atum-martin on 12/05/2017.
+ */
+
+public class YoutubeHttp {
+
+    public void openUrl(String url){
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestProperty("accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+            conn.setRequestProperty("accept-encoding","");
+            conn.setRequestProperty("accept-language","en-US,en;q=0.8");
+            conn.setRequestProperty("referer","https://www.youtube.com/");
+            conn.setRequestProperty("upgrade-insecure-requests","1");
+            conn.setRequestProperty("user-agent:","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36");
+
+            String line;
+            int returnCode = conn.getResponseCode();
+            System.out.println("return code: "+returnCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
