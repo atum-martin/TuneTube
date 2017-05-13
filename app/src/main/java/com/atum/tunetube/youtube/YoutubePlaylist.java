431aaf2cd11db515431b61dfd15827b127c049d4
Martin <martin.chapman4@mail.dcu.ie>
2017-05-13 23:24:39 +0100

    Added a method to return a youtube video POJO based on a supplied youtube playlist url.

---
+++ app/src/main/java/com/atum/tunetube/youtube/YoutubePlaylist.java
@@ -0,0 +1,25 @@
+package com.atum.tunetube.youtube;
+
+import java.util.List;
+
+/**
+ * Created by Admin on 13/05/2017.
+ */
+public class YoutubePlaylist {
+
+    public void get(YoutubeHttp http){
+        List<String> content = http.openUrl("");
+        for(String line : content){
+            if(line.contains("yt-lockup-title")){
+                int index = line.indexOf("<a");
+                line = line.substring(index);
+                index = line.indexOf("href=\"");
+                line = line.substring(index+6);
+                index = line.indexOf("\"");
+                String videoId = line.substring(0, index);
+                String title = line.substring(line.indexOf(">")+1,line.indexOf("</a>"));
+
+            }
+        }
+    }
+}
